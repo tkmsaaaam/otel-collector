3fe5c6b9ac2cdc93c7cd98a1bfce297f76860f3e
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-07-27 21:15:50 +0900

    fix test cases

--- processor/traceBufferRedis/buffer_test.go
+++ processor/traceBufferRedis/buffer_test.go
@@ -157,7 +157,7 @@ func TestIsContinue(t *testing.T) {
 		t     time.Time
 	}
 	start := time.Date(2024, 7, 10, 0, 0, 0, 0, &time.Location{})
-	end := time.Date(2024, 7, 11, 0, 0, 0, 0, &time.Location{})
+	end := time.Date(2024, 7, 13, 0, 0, 0, 0, &time.Location{})
 	tests := []struct {
 		name  string
 		param Param
@@ -165,37 +165,47 @@ func TestIsContinue(t *testing.T) {
 	}{
 		{
 			name:  "v is nil",
-			param: Param{v: nil, start: &start, end: &end, t: time.Date(2024, 7, 10, 12, 0, 0, 0, &time.Location{})},
+			param: Param{v: nil, start: &start, end: &end, t: time.Date(2024, 7, 11, 0, 0, 0, 0, &time.Location{})},
 			want:  true,
 		},
 		{
 			name:  "v.Time is before",
-			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 10, 11, 59, 59, 59, &time.Location{})}, start: &start, end: &end, t: time.Date(2024, 7, 10, 12, 0, 0, 0, &time.Location{})},
-			want:  true,
-		},
-		{
-			name:  "start is before",
-			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 9, 23, 59, 59, 59, &time.Location{})}, start: &start, end: &end, t: time.Date(2024, 7, 10, 12, 0, 0, 0, &time.Location{})},
+			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 11, 0, 0, 0, 0, &time.Location{})}, start: &start, end: &end, t: time.Date(2024, 7, 12, 0, 0, 0, 0, &time.Location{})},
 			want:  true,
 		},
 		{
 			name:  "start is nil",
-			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 10, 11, 59, 59, 59, &time.Location{})}, start: nil, end: &end, t: time.Date(2024, 7, 10, 12, 0, 0, 0, &time.Location{})},
-			want:  true,
+			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 12, 0, 0, 0, 0, &time.Location{})}, start: nil, end: &end, t: time.Date(2024, 7, 11, 0, 0, 0, 0, &time.Location{})},
+			want:  false,
 		},
 		{
-			name:  "end is after",
-			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 12, 0, 0, 0, 1, &time.Location{})}, start: &start, end: &end, t: time.Date(2024, 7, 10, 12, 0, 0, 0, &time.Location{})},
+			name:  "start is before",
+			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 12, 0, 0, 0, 0, &time.Location{})}, start: &start, end: &end, t: time.Date(2024, 7, 11, 0, 0, 0, 0, &time.Location{})},
+			want:  false,
+		},
+		{
+			name:  "start is after",
+			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 9, 0, 0, 0, 0, &time.Location{})}, start: &start, end: &end, t: time.Date(2024, 7, 8, 0, 0, 0, 0, &time.Location{})},
 			want:  true,
 		},
 		{
 			name:  "end is nil",
-			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 11, 11, 59, 59, 59, &time.Location{})}, start: &start, end: nil, t: time.Date(2024, 7, 11, 12, 0, 0, 0, &time.Location{})},
+			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 12, 0, 0, 0, 0, &time.Location{})}, start: &start, end: nil, t: time.Date(2024, 7, 11, 0, 0, 0, 0, &time.Location{})},
+			want:  false,
+		},
+		{
+			name:  "end is after",
+			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 12, 0, 0, 0, 0, &time.Location{})}, start: &start, end: &end, t: time.Date(2024, 7, 11, 0, 0, 0, 0, &time.Location{})},
+			want:  false,
+		},
+		{
+			name:  "end is before",
+			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 14, 0, 0, 0, 0, &time.Location{})}, start: &start, end: &end, t: time.Date(2024, 7, 8, 0, 0, 0, 0, &time.Location{})},
 			want:  true,
 		},
 		{
 			name:  "false",
-			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 11, 11, 59, 59, 59, &time.Location{})}, start: &start, end: nil, t: time.Date(2024, 7, 10, 12, 0, 0, 0, &time.Location{})},
+			param: Param{v: &TraceMetadata{Id: pcommon.NewTraceIDEmpty(), Time: time.Date(2024, 7, 10, 12, 1, 1, 1, &time.Location{})}, start: &start, end: &end, t: time.Date(2024, 7, 10, 12, 0, 0, 0, &time.Location{})},
 			want:  false,
 		},
 	}
